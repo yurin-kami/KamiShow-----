61a97c0d96afed77a9d2eb72bc35bc157e9e2638
yurin He <123640115+yurin-kami@users.noreply.github.com>
2025-11-06 12:41:37 +0800

    Delete .next/dev directory

--- .next/dev/static/chunks/app_layout_3873bb71.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_c95b6a11._.js",
-  "static/chunks/app_globals_71f961d1.css"
-],
-    source: "dynamic"
-});
